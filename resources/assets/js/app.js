4e763477c96ec9e88c2b2747d5e2b1f2f395e080
Eric Closquet <closquet.eric@live.be>
2018-08-21 23:30:05 +0200

    add vue-resource

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,17 @@
 require('./bootstrap');
 
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 
-import Root from './Root.vue';
+Vue.use(VueResource);
+
+import AdminRoot from './components/AdminRoot.vue';
+import GuestRoot from './components/GuestRoot.vue';
 
 const app = new Vue({
+    components:{
+        AdminRoot,
+        GuestRoot,
+    },
     el: '#app',
-    render: h => h(Root)
 });
