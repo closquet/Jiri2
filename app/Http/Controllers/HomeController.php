5ae74abcc8ca1314c9d7a3f5fe35c46e723b8433
Eric Closquet <closquet.eric@live.be>
2018-08-23 18:45:04 +0200

    add $user ( Auth::user() ) to index() response

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,6 @@ class HomeController extends Controller
     public function index()
     {
 	    $user = Auth::user();
-        return view('home', $user);
+        return view('home', [ 'user' => $user ]);
     }
 }
