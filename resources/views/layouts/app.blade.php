2729f51dc68b143f71ca160e0b7c9bc7d3f0a2f1
Eric Closquet <closquet.eric@live.be>
2018-08-23 18:46:23 +0200

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="favicon.png" sizes="128x128">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
