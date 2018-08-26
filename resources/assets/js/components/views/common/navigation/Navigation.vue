097c0405f8f2a4b2aa8d8a5497c2ed3a2639a5cd
Eric Closquet <closquet.eric@live.be>
2018-08-26 23:04:13 +0200

    add main nav in vuejs

--- resources/assets/js/components/views/common/navigation/Navigation.vue
+++ resources/assets/js/components/views/common/navigation/Navigation.vue
@@ -1,18 +1,41 @@
 <template>
-    <div class="nav-scroller bg-white shadow-sm">
-        <nav aria-labelledby="main-nav-title" role="navigation" class="container nav nav-underline">
-            <h2 id="main-nav-title" role="heading" aria-level="2" class="sr-only">
-                Menu Principal
-            </h2>
+    <div>
+        <div class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <h1 class="navbar-brand site-title mr-auto" role="heading" aria-level="1">
+                    <router-link to="/home">
+                        Jiri
+                    </router-link>
+                </h1>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <router-link class="nav-link" to="/home">home</router-link>
-            <router-link class="nav-link" to="/testc">common link</router-link>
-
-            <router-link v-if="currentUser.is_admin" class="nav-link" to="/testa">testa</router-link>
-
-            <router-link v-if="!currentUser.is_admin" class="nav-link" to="/testg">testg</router-link>
-        </nav>
+                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Projets
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown01">
+                                <router-link class="dropdown-item" to="/projets/ajouter">Ajouter</router-link>
+                                <router-link class="dropdown-item" to="/projets">Tous les projets</router-link>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{currentUser.email}}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown02">
+                                <a class="dropdown-item" href="/logout">Se déconnecter</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
