2d08a1b68658295a92fd01dcc18d02dc1ccb7081
Eric Closquet <closquet.eric@live.be>
2018-08-30 10:39:30 +0200

    add ContentHeader view part

---
+++ resources/assets/js/components/views/common/contentHeader/ContentHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 class="pb-2 mt-4 mb-3 border-bottom">
+            <slot name="title"></slot>
+        </h1>
+        <div class="row mb-4">
+            <div class="col-md-10 col-lg-8 col-xl-6">
+                <slot name="content"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex';
+
+    export default {
+        computed:{
+            ...mapState('user',[
+                'currentUser',
+            ]),
+        },
+    }
+</script>
