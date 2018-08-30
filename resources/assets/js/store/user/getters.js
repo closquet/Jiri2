e369b082d52c6f9ae1aeb4edb607b8f5b20f3d77
Eric Closquet <closquet.eric@live.be>
2018-08-30 11:49:11 +0200

    add getters: getAvailableGuests, getAvailableteachers, getUnavailableUsers, getAvailableGuests, getAvailableteachers, getUnavailableUsers, getUnavailableGuests, getUnavailableteachers, getUserFromId

--- resources/assets/js/store/user/getters.js
+++ resources/assets/js/store/user/getters.js
@@ -3,9 +3,20 @@ import api from './api'
 const getters = {
 
     getAllUsers: state => state.users,
-    getAllAdminUsers: state => state.users.filter(user => user.isAdmin),
-    getAllGuestUsers: state => state.users.filter(user => !user.isAdmin),
-    getAvailableUsers: state => state.users.filter(user => user.isAvailable),
+
+    getAllAdminUsers: state => state.users.filter(user => user.is_admin),
+    getAllGuestUsers: state => state.users.filter(user => !user.is_admin),
+
+    getAvailableUsers: state => state.users.filter(user => user.is_available),
+    getAvailableGuests: state => state.users.filter(user => user.category === 2 && user.is_available),
+    getAvailableteachers: state => state.users.filter(user => user.category === 1 && user.is_available),
+
+    getUnavailableUsers: state => state.users.filter(user => !user.is_available),
+    getUnavailableGuests: state => state.users.filter(user => user.category === 2 && !user.is_available),
+    getUnavailableteachers: state => state.users.filter(user => user.category === 1 && !user.is_available),
+
+    getUserFromId: state => userId => state.users.find(user => user.id === userId),
+
     getCurrentUser: state => state.currentUser,
 };
 
