a7dcd0aa8d3beabd5b753c827408cdd28c222e6a
Eric Closquet <closquet.eric@live.be>
2018-08-30 10:24:02 +0200

    add UsersList view and its three children

---
+++ resources/assets/js/components/views/admin/users/UsersList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container site-content">
+        <Content-header>
+            <template slot="title">
+                Gestion des utilisateurs <router-link to="/utilisateurs/ajouter" class="btn btn-primary">Ajouter</router-link>
+            </template>
+            <template slot="content">
+                <p>
+                    Vous pouvez choisir de les afficher <router-link to="/utilisateurs">tous</router-link>,- de n'afficher que les <router-link to="/utilisateurs/professeurs">professeurs</router-link>, ou encore, que les <router-link to="/utilisateurs/guests">examinateurs invités</router-link>.
+                </p>
+            </template>
+        </Content-header>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import ContentHeader from '../../common/contentHeader/ContentHeader'
+    import {mapState, mapActions} from 'vuex';
+
+    export default {
+        components:{
+            ContentHeader,
+        },
+        computed:{
+            ...mapState('user',[
+                'users ',
+            ]),
+        },
+        methods:{
+            ...mapActions('user',[
+                'storeAllUsers',
+            ]),
+        },
+        beforeMount(){
+            this.storeAllUsers();
+        },
+    }
+</script>
---
+++ resources/assets/js/components/views/admin/users/parts/GuestsList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <section class="mb-5" aria-labelledby="dashboard-current-jury-title">
+            <h2 class="mb-3" id="dashboard-current-jury-title" role="heading" aria-level="2">
+                Les personnes invitées
+            </h2>
+            <div class="card">
+                <div class="card-body">
+                    <span class="h5 card-title" role="heading" aria-level="3">Disponibles</span>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li v-for="user in getAvailableGuests" class="list-group-item">
+                        <a @click.prevent="disableUser(user.id)" title="Déplacer dans indisponible" href="#"><font-awesome-icon icon="minus-circle" class="mr-1" /></a>
+                        <router-link :to="'/utilisateurs/' + user.id + '/modifier'" title="Modifier" href="#"><font-awesome-icon icon="edit" class="mr-1" /></router-link>
+                        {{user.name}} - {{user.email}}{{user.is_admin ? ' - ADMINISTRATEUR' : ''}}
+                    </li>
+                </ul>
+                <div class="card-body">
+                    <span class="h5 card-title" role="heading" aria-level="3">Indisponible</span>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li v-for="user in getUnavailableGuests" class="list-group-item list-group-item-dark">
+                        <a @click.prevent="enableUser(user.id)" title="Déplacer dans disponible" href="#"><font-awesome-icon icon="plus-circle" class="mr-1" /></a>
+                        <router-link :to="'/utilisateurs/' + user.id + '/modifier'" title="Modifier" href="#"><font-awesome-icon icon="edit" class="mr-1" /></router-link>
+                        {{user.name}} - {{user.email}}{{user.is_admin ? ' - ADMINISTRATEUR' : ''}}
+                    </li>
+                </ul>
+            </div>
+            <router-link to="/utilisateurs/ajouter" class="btn btn-primary">Ajouter</router-link> <a href="#" @click.prevent="$router.go(-1)" class="mr-2 mt-2 btn btn-link">Retour</a>
+        </section>
+    </div>
+</template>
+
+<script>
+    import { mapGetters, mapActions} from 'vuex';
+
+    export default {
+        computed: {
+            ...mapGetters('user',[
+                'getAvailableGuests',
+                'getUnavailableGuests',
+            ]),
+        },
+        methods:{
+            ...mapActions('user',[
+                'disableUser',
+                'enableUser',
+            ]),
+        },
+    }
+</script>
