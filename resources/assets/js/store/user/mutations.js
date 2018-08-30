e126774e37c60483e7bf9c6fb4f8ff038cf50b43
Eric Closquet <closquet.eric@live.be>
2018-08-30 11:55:31 +0200

    add space in file

--- resources/assets/js/store/user/mutations.js
+++ resources/assets/js/store/user/mutations.js
@@ -2,18 +2,23 @@ const mutations = {
     STORE_CURRENT_USER: (state, currentUser) => { //used in routes.js
         state.currentUser = currentUser;
     },
+
     STORE_ALL_USERS: (state, users) => {
         state.users = users;
     },
+
     DISABLE_USER: (state, userId) => {
         state.users.filter(user => user.id === userId)[0].isAvailable = 0
     },
+
     ENABLE_USER: (state, userId) => {
         state.users.filter(user => user.id === userId)[0].isAvailable = 1
     },
+
     UPDATE_USER: (state, newUserContent) => {
         state.users.filter(user => user.id === newUserContent.id)[0] = newUserContent;
     }
+
     ADD_USER: (state, {name, email, phone, category, isAvailable, isAdmin}) => {
         state.users.push({name, email, phone, category, isAvailable, isAdmin});
     },
