a8c3a3d49b934188c1fddd0d8110f6e92eb088dc
Eric Closquet <closquet.eric@live.be>
2018-08-30 11:34:20 +0200

    add AddUser view

---
+++ resources/assets/js/components/views/admin/users/AddUser.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container site-content">
+        <Content-header>
+            <template slot="title">
+                Nouvel utilisateur
+            </template>
+        </Content-header>
+        <UserForm></UserForm>
+    </div>
+</template>
+
+<script>
+    import ContentHeader from '../../common/contentHeader/ContentHeader';
+    import UserForm from './parts/UserForm';
+
+    export default {
+        components:{
+            ContentHeader,
+            UserForm,
+        },
+    }
+</script>
