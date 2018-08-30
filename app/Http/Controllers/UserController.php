bb03f95ea293908e29ebdc30e8de3208a18e86a2
Eric Closquet <closquet.eric@live.be>
2018-08-30 10:12:00 +0200

    add methods in user controller.
    methods: enable, disable, update, (store, not finished)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,9 @@
 namespace Jiri\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Jiri\User;
+use Validator;
 
 class UserController extends Controller
 {
@@ -28,14 +30,31 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Disable the specified resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function disable($id)
     {
-        //
+    	$user = User::findOrFail($id);
+    	$user->is_available = 0;
+    	$user->save();
+        return response('user ' . $user->name . ' disabled in the database', 200);
+    }
+
+    /**
+     * Enable the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function enable($id)
+    {
+    	$user = User::findOrFail($id);
+    	$user->is_available = 1;
+    	$user->save();
+        return response('user ' . $user->name . ' enabled in the database', 200);
     }
 
     /**
@@ -50,17 +69,6 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -69,8 +77,61 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+	    $validator = Validator::make($request->all(), [
+		    'name' => 'required|max:20',
+		    'email' => 'required|email',
+		    'phone' => 'nullable',
+		    'category' => 'required|in:0,1,2',
+		    'password' => 'required_if:password_confirmation,nullable',
+		    'password_confirmation' => 'required_if:password,nullable|same:password',
+		    'is_available' => 'required|boolean',
+		    'is_admin' => 'required|boolean',
+	    ]);
+	
+	    if ($validator->fails()) {
+	    	return response($validator->errors(), 304);
+	    }
+	    
+	    $user = User::findOrFail($id);
+	    $user->name = $request->name;
+	    $user->email = $request->email;
+	    $user->phone = $request->phone;
+	    $user->category = $request->category;
+	    
+	    if( $request->password != null && $request->password != '' ){
+	    	if($request->password !== $request->password_confirmation){
+	    		return response('passwords didn\'t match', 304);
+	    	}
+	    	if(strlen( $request->password) < 6){
+	    		return response('minimum 6 characters for password', 304);
+	    	}
+		    $user->password = Hash::make($request->password);
+	    }
+	    
+	    $user->is_available = $request->is_available;
+	    $user->is_admin = $request->is_admin;
+	    $user->save();
+	    return response('user ' . $user->name . ' updated in the database', 200);
     }
+	
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$user = new User();
+		$user->name = $request->name;
+		$user->email = $request->email;
+		$user->phone = $request->phone;
+		$user->category = $request->category;
+		$user->is_available = $request->is_available;
+		$user->is_admin = $request->is_admin;
+		$user->save();
+		return response('user ' . $user->name . ' saved in the database', 200);
+	}
 
     /**
      * Remove the specified resource from storage.
