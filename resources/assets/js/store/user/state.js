6d0765b96d76096bdc0a294973de094ffd6c2a74
Eric Closquet <closquet.eric@live.be>
2018-08-30 12:27:25 +0200

    remove test content

--- resources/assets/js/store/user/state.js
+++ resources/assets/js/store/user/state.js
@@ -1,15 +1,5 @@
 const state = {
-    users:[
-        {
-            id:'',
-            name:'',
-            category:'1', //1 = teacher, 2 = guest
-            email:'',
-            phone:'',
-            isAdmin:'0',
-            isAvailable:'0'
-        },
-    ],
+    users:[],
     currentUser:{
         id:'',
         name:'',
