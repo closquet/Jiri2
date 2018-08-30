4ff07709a79be15ceacc661d64a1860c7d24bfc3
Eric Closquet <closquet.eric@live.be>
2018-08-30 10:38:53 +0200

    add UserEdit view

---
+++ resources/assets/js/components/views/admin/users/UserEdit.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container site-content">
+        <Content-header>
+            <template slot="title">
+                Profile de {{ getUserFromId(parseInt(this.$route.params.userId)).name }}
+            </template>
+        </Content-header>
+        <UserForm :user="getUserFromId(parseInt(this.$route.params.userId))"></UserForm>
+    </div>
+</template>
+
+<script>
+    import ContentHeader from '../../common/contentHeader/ContentHeader';
+    import UserForm from './parts/UserForm';
+    import {mapGetters} from 'vuex';
+
+    export default {
+        components:{
+            ContentHeader,
+            UserForm,
+        },
+        computed:{
+            ...mapGetters('user', [
+                'getUserFromId'
+            ]),
+        },
+    }
+</script>
