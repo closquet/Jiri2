287cab33a94e54790a70f7bed698fa71b3e926cc
Eric Closquet <closquet.eric@live.be>
2018-08-30 10:18:01 +0200

    add fortawesome/vue-fontawesome add some icons

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,28 @@ import store from './store'
 import AdminRoot from './components/views/admin/AdminRoot.vue';
 import GuestRoot from './components/views/guest/GuestRoot.vue';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import {
+    faPlusCircle,
+    faMinusCircle,
+    faEdit,
+    faThumbsUp,
+    faThumbsDown,
+    faSpinner
+} from '@fortawesome/free-solid-svg-icons';
+library.add(
+    faPlusCircle,
+    faMinusCircle,
+    faEdit,
+    faThumbsUp,
+    faThumbsDown,
+    faSpinner
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 const app = new Vue({
     store,
     components:{
