26021c35280e1323a2e5d607af7b0e6d5576a3cc
Eric Closquet <closquet.eric@live.be>
2018-08-30 11:44:11 +0200

    add true content in addUserrequest (axios request)

--- resources/assets/js/store/user/api.js
+++ resources/assets/js/store/user/api.js
@@ -47,8 +47,30 @@ export const getCurrentUser = () => {
 };
 
 //if logged and is admin
-export const addUser = (userToAdd) => {
-    return true;
+export const disableUser = (userId, cb) => {
+    axios.post('/users/' + userId + '/disable')
+        .catch( error => {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+            cb(error.response.status);
+        })
+        .then( axiosResponse => {
+            cb(axiosResponse.status);
+        });
 };
 
 //if logged and is admin
