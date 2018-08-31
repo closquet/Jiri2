5db8835e498a9f156a8258f2bc16b44f62da2384
Eric Closquet <closquet.eric@live.be>
2018-08-31 17:12:19 +0200

    edit all the axios request, moved then() to the top

--- resources/assets/js/store/user/api.js
+++ resources/assets/js/store/user/api.js
@@ -40,12 +40,15 @@ export const getCurrentUser = () => {
 //if logged and is admin
 export const disableUser = (userId, cb) => {
     axios.post('/users/' + userId + '/disable')
+        .then( axiosResponse => {
+            cb(axiosResponse.status);
+
+        })
         .catch( error => {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 console.log(error.response.data);
-                console.log(error.response.status);
                 console.log(error.response.headers);
             } else if (error.request) {
                 // The request was made but no response was received
@@ -58,21 +61,20 @@ export const disableUser = (userId, cb) => {
             }
             console.log(error.config);
             cb(error.response.status);
-        })
-        .then( axiosResponse => {
-            cb(axiosResponse.status);
         });
 };
 
 //if logged and is admin
 export const enableUser = (userId, cb) => {
     axios.post('/users/' + userId + '/enable')
+        .then( axiosResponse => {
+            cb(axiosResponse.status);
+        })
         .catch( error => {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 console.log(error.response.data);
-                console.log(error.response.status);
                 console.log(error.response.headers);
             } else if (error.request) {
                 // The request was made but no response was received
@@ -85,21 +87,20 @@ export const enableUser = (userId, cb) => {
             }
             console.log(error.config);
             cb(error.response.status);
-        })
-        .then( axiosResponse => {
-            cb(axiosResponse.status);
         });
 };
 
 //if logged and is admin
 export const updateUser = (userContent, cb) => {
     axios.post('/users/' + userContent.id + '/update', userContent)
+        .then( axiosResponse => {
+            cb(axiosResponse.status);
+        })
         .catch( error => {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 console.log(error.response.data);
-                console.log(error.response.status);
                 console.log(error.response.headers);
             } else if (error.request) {
                 // The request was made but no response was received
@@ -112,21 +113,20 @@ export const updateUser = (userContent, cb) => {
             }
             console.log(error.config);
             cb(error.response.status);
-        })
-        .then( axiosResponse => {
-            cb(axiosResponse.status);
         });
 };
 
 //if logged and is admin
 export const addUser = (userContent, cb) => {
     axios.post('/users/store', userContent)
+        .then( axiosResponse => {
+            cb(axiosResponse.status);
+        })
         .catch( error => {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 console.log(error.response.data);
-                console.log(error.response.status);
                 console.log(error.response.headers);
             } else if (error.request) {
                 // The request was made but no response was received
@@ -139,8 +139,5 @@ export const addUser = (userContent, cb) => {
             }
             console.log(error.config);
             cb(error.response.status);
-        })
-        .then( axiosResponse => {
-            cb(axiosResponse.status);
         });
 };
