1c72063a2a9210bebb2048d193d8c362a3419b60
Eric Closquet <closquet.eric@live.be>
2018-08-31 17:16:07 +0200

    moved getAvailableteachers to getAvailableTeachers

--- resources/assets/js/store/user/getters.js
+++ resources/assets/js/store/user/getters.js
@@ -8,11 +8,10 @@ const getters = {
     getAvailableUsers: state => state.users.filter(user => user.is_available),
     getAvailableUsersNoSupAdmin: state => state.users.filter(user => user.is_available && user.category !== 3),
     getAvailableGuests: state => state.users.filter(user => user.category === 2 && user.is_available),
-    getAvailableteachers: state => state.users.filter(user => user.category === 1 && user.is_available),
 
     getUnavailableUsers: state => state.users.filter(user => !user.is_available),
     getUnavailableGuests: state => state.users.filter(user => user.category === 2 && !user.is_available),
-    getUnavailableteachers: state => state.users.filter(user => user.category === 1 && !user.is_available),
+    getUnavailableTeachers: state => state.users.filter(user => user.category === 1 && !user.is_available),
 
     getUserFromId: state => userId => state.users.find(user => user.id === userId),
 
