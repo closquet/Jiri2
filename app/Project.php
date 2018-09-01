fa739643970b15357a635faff335519fe14fc88c
Eric Closquet <closquet.eric@live.be>
2018-09-01 21:26:18 +0200

    created the eloquent relationship project_user

--- app/Project.php
+++ app/Project.php
@@ -9,4 +9,9 @@ class Project extends Model
 	protected $fillable = [
 		'name', 'bloc', 'description'
 	];
+	
+	public function student()
+	{
+		return $this->belongsToMany( 'Jiri\Student')->withPivot('site_url', 'site_account', 'site_password', 'github_url', 'is_available')->withTimestamps();;
+	}
 }
--- app/Student.php
+++ app/Student.php
@@ -9,4 +9,11 @@ class Student extends Model
 	protected $fillable = [
 		'name', 'email', 'phone', 'bloc', 'is_available'
 	];
+	
+	public function project()
+	{
+		return $this->belongsToMany( 'Jiri\Project')->withPivot('site_url', 'site_account', 'site_password', 'github_url', 'is_available')->withTimestamps();;
+	}
+	
+	
 }
